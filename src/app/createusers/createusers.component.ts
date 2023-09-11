b2e3bfdf3353ec4986cc5ac65724d6505b945a37
Akshithuppula <akshithuppula1234@gmail.com>
2023-09-11 19:40:59 +0530

    complete

--- src/app/createusers/createusers.component.ts
+++ src/app/createusers/createusers.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
+import { FormArray, FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 import { UsersService } from '../users.service';
 
 @Component({
@@ -10,11 +10,38 @@ import { UsersService } from '../users.service';
 export class CreateusersComponent {
 
   public usersForm: FormGroup = new FormGroup({
-    name: new FormControl(),
-    phone: new FormControl(),
+    name: new FormControl("",[Validators.required,Validators.minLength(3)]),
+    phone: new FormControl("",[Validators.required,Validators.min(1000000000),Validators.max(9999999999)]),
     city: new FormControl(),
     fee: new FormControl(),
+    address: new FormGroup({
+      hno: new FormControl(),
+      state: new FormControl("",[Validators.required]),
+      pin: new FormControl("",[Validators.required,Validators.min(100000),Validators.max(999999)])
+    }),
+    type: new FormControl(),
+    busFee: new FormControl(),
+    hostelFee: new FormControl(),
+    cards: new FormArray([])
     });
+
+    get cardsFormArray(){
+      return this.usersForm.get('cards') as FormArray;
+    }
+
+    addCards(){
+      this.cardsFormArray.push(
+        new FormGroup({
+          no: new FormControl(),
+          exp: new FormControl(),
+          cvv: new FormControl("",[Validators.required,Validators.min(100),Validators.max(999)]),
+        })
+      )
+    }
+
+    deleteCard(i:number){
+      this.cardsFormArray.removeAt(i);
+    }
      
     constructor(private createusersService:UsersService ){}
 
